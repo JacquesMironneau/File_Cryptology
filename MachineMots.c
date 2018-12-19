c0df9f913995d09eb555a6efe602c8c5cb25f987
Thomas Durand <durantho@ltsp3.iut-valence.fr>
2018-12-19 14:01:10 +0100

    je push sut TOMA

--- MachineMots.c
+++ MachineMots.c
@@ -18,29 +18,50 @@ Mot motUsed;
 void demarrerMot(void)
 {
     printf("lancement de démarrer mot\n");
-   demarrerCar();
-   printf("Lancement avancer mot \n");
-   avancerMot();
+    demarrerCar();
+    printf("Lancement avancer mot \n");
+    avancerMot();
 }
 
 void avancerMot(void) // Création d'un mot et enregistrement de sa valeur deans motUsed
-{  
+{  /*
     int i = 0;
-    avancerCar(); // On se rend sur le caractère 
-    printf("On est avant le premier while");
+    //avancerCar(); // On se rend sur le caractère 
+    printf("On est avant le premier while\n");
     while (carCourant() == ' '){ // On avance tant que l'on parcourt des espaces
         avancerCar();
-        printf("Dans le 1er while");
+        printf("%c",carCourant());
+        printf("Dans le 1er while\n");
     }
     while ((carCourant()  != '\n') && (i < 26)){
-        printf("Dans le second while");
+        printf("Dans le second while\n");
+        avancerCar();
         motUsed.tab[i] = carCourant();
-        printf("%c", motUsed.tab[i]);
+        printf("%c\n", motUsed.tab[i]);
         motUsed.lgm++;
+        printf("%c\n",motUsed.lgm);
         i++;
 
 
     }
+    */
+    int inter = 0;/*
+    printf("@here inter = %d\n",inter);
+    printf("@here => before 1st while\n");
+    while (carCourant()==' '){
+        avancerCar();
+        printf("@here carCourant() = %c\n", carCourant());
+    }*/
+    printf("\n\n\n@here => after 1st while and before 2nd while\n\n\n\n");
+
+    while(carCourant() != '\0'){
+        motUsed.tab[inter]=carCourant();
+        motUsed.lgm++;
+        printf("@here motUSed.tab[i] = %c\n",motUsed.tab[inter]);
+        printf("@here carCourant = %c\n", carCourant());
+        printf("@here motUsed.lgm = %c\n",motUsed.lgm);
+        avancerCar();
+    }
 
 }
 
@@ -48,40 +69,7 @@ void avancerMot(void) // Création d'un mot et enregistrement de sa valeur deans
 
 
 
-
-
-
-     // Ouai
-    //Création du mot
-    /*
-
-    if( carCourant() != marque()){ // Tant que le caractère courrant n'est pas la marque
-    // Pas sur de devoir le traiter si ? si.
-    }
-    else{ // On est pas sur la maruqe
-    
-    avancerCar(); // On est forcément sur un caractère ou un \n
-        if (carCourant() == " "){ // Si on est sur un espace
-            avancerCar();
-        }
-        else if(carCourant() == "\n"){ // Si on est sur un caractère
-        motUsed.tab[0] = 'L';
-        motUsed.tab[1] = 'F';
-        motUsed.lgm = 1;
-
-        }
-        else{
-            avancerCar();  
-            motUsed.tab[i] = carCourant();
-            motUsed.lgm++;
-            i++;
-        }
-
-    }*/
-
-
-
-Mot motCour(void) // Quand on l'appelle on motCour().lgm ou motCour().tab[x]
+Mot motCour(void) // Quand on l'appelle on motCour().lgm et motCour().tab[x]
 {
     return motUsed;
 }
